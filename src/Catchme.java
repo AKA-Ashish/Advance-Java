18c9cd81c902b05335d69664a1bf82efc932c6a9
Ashish <meashish2020@gmail.com>
2023-03-26 17:02:58 +0545

    MouseAdapter

--- src/Catchme.java
+++ src/Catchme.java
@@ -3,16 +3,19 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.Random;
 
-public class Catchme implements MouseListener {
+import java.awt.Color;
+
+public class Catchme extends MouseAdapter {
+    
     int x;
     int y;
     JButton jb;
     JLabel label;
-    
+    JFrame f;
 
    
     public Catchme(){
-        JFrame f = new JFrame("Catch The Button If you Can");
+         f = new JFrame("Catch The Button If you Can");
 
          jb = new JButton("Catch For Prize");
          
@@ -42,39 +45,62 @@ public class Catchme implements MouseListener {
         new Catchme();
     }
 
-    int counter;
+    int count;
     @Override
     public void mouseClicked(MouseEvent e) {
-        counter+= 1;
-        label.setText("Total Count :"+ counter );
+        count+= 1;
+        label.setText("Total Count :"+ count );
 
        
        
     }
-    @Override
-    public void mousePressed(MouseEvent e) {
+    // @Override
+    // public void mousePressed(MouseEvent e) {
       
-    }
-    @Override
-    public void mouseReleased(MouseEvent e) {
+    // }
+    // @Override
+    // public void mouseReleased(MouseEvent e) {
      
-    }
+    // }
     @Override
     public void mouseEntered(MouseEvent e) {
+        
         Random random = new Random();
         x = random.nextInt(400);
         y = random.nextInt(400);
 
-        jb.setBounds(x,y,150,30);
+        jb.setBounds(x,y,140,30);
+
+
+        count = random.nextInt(5);
+        System.out.println(count);
+        switch (count) {
+            case 0:
+            f.getContentPane().setBackground(Color.BLACK);
+                break;
+            case 1:
+            f.getContentPane().setBackground(Color.green);
+                break;
+
+            case 2:
+                f.getContentPane().setBackground(Color.cyan);
+                break;
+            case 3:
+            f.getContentPane().setBackground(Color.yellow);
+                break;
+            case 4:
+            f.getContentPane().setBackground(Color.red);
+                break;
         
         
-       
+
+
     }
-    @Override
-    public void mouseExited(MouseEvent e) {
+    // @Override
+    // public void mouseExited(MouseEvent e) {
      
        
-    }
+    // }
     
-    
+    }   
 }
